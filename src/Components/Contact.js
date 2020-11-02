ea27beefc2dcef7194dc0bb9defdb7ba4f5ad05b
JoshuaRotimi <62189959+JoshuaRotimi@users.noreply.github.com>
2020-11-02 10:24:15 +0100

    edited profile message

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -7,11 +7,11 @@ const Contact = ({ data }) => {
    const [email, setEmail] = useState('');
    const [message, setMessage] = useState('');
 
-   console.log(data);
+   // console.log(data);
 
     const handleClick = (e) => {
        e.preventDefault();
-      window.open(`mailto:${email}?subject=${subject}&body=${name}: ${message}`);
+      window.open(`mailto:olorunnipaj@yahoo.com?subject=${subject}&body=${message} : From ${email}`);
     };
     
 
